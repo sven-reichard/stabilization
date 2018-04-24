4a551415ff447f007613b456e3e7b874d2045c2b
Sven Reichard <sven.reichard@tu-dresden.de>
2018-04-24 11:15:33 +0200

    Implement general t-vertex stabilization

--- tensor.cpp
+++ tensor.cpp
@@ -5,11 +5,13 @@
 
 # include <vector>
 # include <iostream>
+# include <fstream>
 # include <iterator>
 # include <cassert>
 # include <string>
 # include <map>
 # include <algorithm>
+# include <numeric>
 typedef std::map<std::vector<int>, int> Multiset;
 
 // perfect hash for vectors of integers less than <order>
@@ -48,6 +50,10 @@ public:
   void setRank(int r)
   {
     rank = r;
+  };
+  int getDimension() const
+  {
+    return dimension;
   };
 private:
   int order;
@@ -90,10 +96,13 @@ void
 tensor::write(std::ostream& output) const
 {
   output << "order " << order << std::endl;
+  
   output << "dimension " << dimension << std::endl;
+  int rank = 0;
   for (unsigned int i = 0; i < data.size(); i++)
     {
       output << data[i] << " ";
+      if (data[i] > rank) rank = data[i];
       // insert newlines for structure
       int k = i+1;
       while (k > 0 && (k%order == 0))
@@ -102,6 +111,7 @@ tensor::write(std::ostream& output) const
 	  k /= order;
 	}
     }
+  output << "rank " << rank+1 <<std::endl;
 }
 
 void expect(const std::string& s1, const std::string& s2)
@@ -158,10 +168,9 @@ bool nextTuple(std::vector<int>& tuple, int maxValue)
 
 void firstSet(std::vector<int>& set)
 {
-  int s = set.size();
-  for (int i = 0; i < s; i++)
-    set[i] = i;
+  std::iota(set.begin(), set.end(), 0);
 };
+
 bool nextSet(std::vector<int>& set, int maxValue)
 // assume that set is an increasing list of values
 // between 0 and maxValue-1
@@ -182,15 +191,13 @@ bool nextSet(std::vector<int>& set, int maxValue)
 
 void printVector(const std::vector<int>& v)
 {
-  std::copy(v.begin(), v.end(),
-	    std::ostream_iterator<int>(std::cout, " "));
+  std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
   std::cout << std::endl;
 }
 
 std::vector<int>
 apply(const std::vector<int>& v, const std::vector<int>& w)
 {
-
   int s = w.size();
   std::vector<int> result(s);
   for (int i = 0; i < s; i++)
@@ -201,87 +208,165 @@ apply(const std::vector<int>& v, const std::vector<int>& w)
 void
 printMultiset(const Multiset& multiset)
 {
-  for (Multiset::const_iterator it = multiset.begin();
-       it != multiset.end();
-       it++)
+  for (auto it = multiset.begin(); it != multiset.end(); it++)
     {
       std::cout << it->second<<" * ";
       printVector(it->first);
     }
 };
 
+typedef std::map<std::vector<int>, int> Multiset;
+
+Multiset collectColors(int t, const tensor& aTensor,
+		       const std::vector<int>& edge)
+{
+  Multiset multiset;
+  const int numberOfColors = t*(t-1)/2; // binomial coefficient
+  const int k = aTensor.getDimension();
+  std::vector<int> tuple(t);
+  std::copy(edge.begin(), edge.end(), tuple.begin());
+  int n = aTensor.getOrder();
+  std::vector<int> aSet(t-k);
+  for (;;)
+    {
+      std::copy(aSet.begin(), aSet.end(), tuple.begin() + k);
+      std::vector<int> colors(numberOfColors);
+      auto color = colors.begin();
+      std::vector<int> coordinates(k);
+      for (firstSet(coordinates); color != colors.end();
+	   color++, nextSet(coordinates, t))
+	{
+	  *color = aTensor[apply(tuple, coordinates)];
+	}
+      multiset[colors] ++;
+      if (!nextTuple(aSet, n))
+	break;
+    }
+  return multiset;
+};
+
+// we use this to collect invariants
+// we construct an injective function T->N
+// if the object is know we return the known value
+// else we assign a new value.
+template <typename T>
+class collection
+{
+  std::vector<T> data;
+public:
+  int operator[]( const T& multiset)
+  // should use hashing
+  {
+    for (unsigned int i = 0; i < data.size(); i++)
+      if (multiset == data[i])
+	return i;
+    data.push_back(multiset);
+    return data.size() - 1;
+  };
+};
+
 void
 collect( tensor& aTensor, int t)
 {
-  const int numberOfColors = t*(t-1)/2; // binomial coefficient
-  typedef std::map<std::vector<int>, int> Multiset;
-  std::vector<Multiset> allResults;
-  std::vector<int> pair(2);
+  const int k = aTensor.getDimension();
+  const int n = aTensor.getOrder();
+  collection<Multiset> results;
+  std::vector<int> edge(k);
   tensor newTensor(aTensor);
-  int n = aTensor.getOrder();
-  for (pair[0]= 0; pair[0] < n; pair[0]++)
-    for (pair[1] = 0; pair[1] < n; pair[1] ++)
-      {
-	std::vector<int> triple(t);
-	triple[0] = pair[0];
-	triple[1] = pair[1];
-	
-	Multiset multiset;
-	int n = aTensor.getOrder();
-	std::vector<int> aSet(t-2);
-	for (;;)
-	  {
-	    for (int i = 0; i < t-2; i++)
-	      triple[i+2] = aSet[i];
-	    std::vector<int> colors(numberOfColors);
-	    auto color = colors.begin();
-	    std::vector<int> coordinates(2);
-	    for (firstSet(coordinates); color != colors.end();
-		 color++, nextSet(coordinates, t))
-	      {
-		*color = aTensor[apply(triple, coordinates)];
-	      }
-	    multiset[colors] ++;
-	    if (!nextTuple(aSet, n))
-	      break;
-	  }
-	int position;
-	auto pointer = std::find(allResults.begin(), allResults.end(), multiset);
-	if (pointer == allResults.end())
-	  {
-	    position = allResults.size();
-	    allResults.push_back(multiset);
-	  }
-	else
-	  position = pointer - allResults.begin();
-	newTensor[pair] = position;
-      }
-  newTensor.setRank(allResults.size());
+  
+  do
+  {
+    Multiset multiset = collectColors(t,  aTensor, edge);
+    newTensor[edge] = results[multiset];
+  } while (nextTuple(edge, n));
   std::swap(aTensor, newTensor);
+}
+
+void
+stabilize(tensor& aTensor, int maximalT)
+{
+  
+  int oldrank;
+  int t = 2;
+  do {
+    oldrank = aTensor.getRank();
+    std::cout<<"t = " << t << std::endl;
+    collect(aTensor, t);
+    std::cout << "rank: " << aTensor.getRank() << std::endl;
+    if (oldrank == aTensor.getRank())
+      t++;
+    else
+      t = 2;
+  } while (t <= maximalT);
+}
+
+void
+writeVector(const std::vector<int>& v)
+{
+  std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+  std::cout <<std::endl;
+}
+
+tensor
+increaseDimension(const tensor& aTensor)
+{
+  tensor result;
+  const int n = aTensor.getOrder();
+  const int k = aTensor.getDimension();
+  result.resize(n, k+1);
+  std::vector<int> coordinates(k+1);
+  collection<std::vector<int> > allTuples;
+  do {
+    std::vector<int> colors;
+    colors.reserve((k+1)*k/2);
+    std::vector<int> edge(k);
+    std::vector<int> set(k);
+    firstSet(set);
+    do {
+      edge = apply(coordinates, set);
+      colors.push_back(aTensor[edge]);
+    } while (nextSet(set, k+1));
+    
+    result[coordinates] = allTuples[colors];
+  } while (nextTuple(coordinates, n));
+  return result;
+}
+
+tensor
+decreaseArity(const tensor& aTensor)
+{
+  tensor result;
+  const int n = aTensor.getOrder();
+  const int k = aTensor.getDimension();
+  result.resize(n, k-1);
+  std::vector<int> coordinates(k-1);
+  collection<std::map<int, int>> allResults;
+  do {
+    std::vector<int> tuple(k);
+    std::copy(coordinates.begin(), coordinates.end(), tuple.begin());
+    std::map<int, int> m;
+    for (tuple.back() = 0; tuple.back() < n; tuple.back() ++)
+      m[aTensor[tuple]] ++;
+    result[coordinates] = allResults[m];
+  } while (nextTuple(coordinates, n));
+  return result;
 }
 
 int main(void)
 {
-  int maximalT = 4;
+  std::ifstream input("Examples/4.1.wl");
   tensor aTensor;
-  aTensor.read(std::cin);
-  if (true)
-    {
-      int oldrank;
-      int t = 2;
-      do {
-	oldrank = aTensor.getRank();
-	std::cout<<"t = " << t << std::endl;
-	collect(aTensor, t);
-	std::cout << "rank: " << aTensor.getRank() << std::endl;
-	if (oldrank == aTensor.getRank())
-	  t++;
-	else
-	  {
-	    t = 2;
-	  }
-      } while (t <= maximalT);
-    }
+  aTensor.read(input);
+    aTensor = increaseDimension(aTensor);
   aTensor.write(std::cout);
+  std::cout << "--------------------" << std::endl;
+  collect(aTensor, 4);
+  aTensor.write(std::cout);
+  std::cout << "--------------------" << std::endl;
+  aTensor = decreaseArity(aTensor);
+  aTensor.write(std::cout);
+  std::cout << "--------------------" << std::endl;
+  //aTensor = decreaseArity(aTensor);
+  //aTensor.write(std::cout);
   return 0;
 };
